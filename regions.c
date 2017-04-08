eb2e75e56c04480f828b0a3384236ff121a739bc
Saad <saadbennmushtaq@gmail.com>
2017-04-08 12:50:30 -0500

    Add rinit() routine in regions.c
    Initialize the memory region by looking for the region name and
    creating a memory region based on the result returned by search().

--- regions.c
+++ regions.c
@@ -6,7 +6,30 @@
 
 
 Boolean rinit( const char *region_name, r_size_t region_size ) {
-
+    
+    Boolean rc = false;
+    
+    if ( region_size > 0 ) {
+        assert( region_size > 0);
+        region_size = (region_size + 7) - ((region_size + 7) % 8); // round up
+        
+        Boolean result = search( region_name );
+        
+        if ( !result ) {
+        	assert( !result);
+            Boolean resIn = insert( region_name, region_size );
+            
+            if( resIn ) {
+            	assert( resIn );
+                rc = true;
+                Boolean rch = rchoose( region_name );
+                assert( rch );
+            }
+        }
+    }
+    
+    return rc;
+    
 } // rinit
   
 
